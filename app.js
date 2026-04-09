9d0379c2b76251db3d3259852d24ca52dba03b5a
AntonioLive8166 <jhernandezaceves33@gmail.com>
2026-04-09 14:12:18 -0600

    Changed app.js to support DB change.

--- app.js
+++ app.js
@@ -2,108 +2,115 @@ $(document).ready(function()
   {
   // Comienza el cuerpo de ready...
   console.log('JQuerry is working');
-  let edit=false;
+  let edit = false;
   
   $('#tarea-result').hide();
   TraerTareas();
   
-  $('#busx').keyup(function(e)
-    {
+  $('#busx').keyup(function(e) {
     console.log($('#busx').val());
     
-    if($('#busx').val()){
+    if($('#busx').val()) {
       let busx = $('#busx').val();
       $.ajax({
         url:'tarea-buscar.php',
         type:'POST',
         data: {busx},
-        success: function(response){
+        success: function(response) {
         //console.log(response);
-          
-          let tasks=JSON.parse(response);
+          let tasks = JSON.parse(response);
           let template = '';
-          tasks.forEach(task=>{
-              template +=`<tr><td> ${task.nom}</td></tr>`;
-            });
-            $('#contenedor').html(template);
-            $('#tarea-result').show();
-          }
-        })
+          tasks.forEach(
+            task => {template += `<tr><td> ${task.nombre}</td></tr>`;}
+          );
+          $('#contenedor').html(template);
+          $('#tarea-result').show();
         }
-    })
+      })
+    }
+  })
   
- $('#tareas-forma').submit(function(e){
-  const postData = {
-        nom: $('#nom').val(),
-        des: $('#des').val(),
-        id: $('#tareaId').val()
-        };
-        let url= edit === false ? 'tareas-agregar.php' : 'tarea-edit.php';
-         console.log(url);
-        $.post(url, postData, function (response) {
-          console.log(response);
-            TraerTareas();
-            $('#tareas-forma').trigger('reset');
-            edit= false;
-            });
-       e.preventDefault(); 
-   });
+  $('#tareas-forma').submit(function(e){
+    const postData = {
+      id: $('#tareaId').val(),
+      entidad: $('#entidad').val(),
+      appat: $('#appat').val(),
+      apmat: $('#apmat').val(),
+      nombre: $('#nombre').val(),
+      fregis: $('#fregis').val()
+    };
+
+    let url = edit === false ? 'tareas-agregar.php' : 'tarea-edit.php';
+    console.log(url);
+
+    $.post(url, postData, function (response) {
+      console.log(response);
+      TraerTareas();
+      $('#tareas-forma').trigger('reset');
+      edit= false;
+    });
+    e.preventDefault(); 
+  });
  // Borar tareas
  
- $(document).on('click','.task-delete',function (){
-      let element=$(this)[0].parentElement.parentElement;
-      let id=$(element).attr("tareaId");
-      $.post('tarea-borrar.php',{id},function(response){
-        TraerTareas();
-        })
-      });
+  $(document).on('click','.task-delete',function (){
+    let element=$(this)[0].parentElement.parentElement;
+    let id=$(element).attr("tareaId");
+    $.post('tarea-borrar.php',{id},function(response){
+      TraerTareas();
+    })
+  });
  
- // Dar clic en alguna de las tareas
- $(document).on('click','.task-item',function (){
+  // Dar clic en alguna de las tareas
+  $(document).on('click','.task-item',function (){
+    let element=$(this)[0].parentElement.parentElement;
+    let id = $(element).attr("tareaId");
     
-     let element=$(this)[0].parentElement.parentElement;
-      let id=$(element).attr("tareaId");
-      
-    
-      $.post('tarea-simple.php',{id},function(response){
-        const task = JSON.parse(response);
-        $('#tareaId').val(task[0].id);
-        $('#nom').val(task[0].nom);
-        $('#des').val(task[0].des);
-        edit=true;
-         })
-        });
+    $.post('tarea-simple.php',{id},function(response){
+      const task = JSON.parse(response);
+      $('#tareaId').val(task[0].id);
+      $('#entidad').val(task[0].entidad);
+      $('#appat').val(task[0].appat);
+      $('#apmat').val(task[0].apmat);
+      $('#nombre').val(task[0].nombre);
+      $('#fregis').val(task[0].fregis);
+      edit = true;
+    })
+  });
 
  
  //////////////////////////////////////////////////////////
-function TraerTareas(){
+  function TraerTareas(){
     $.ajax({
-        url:'tarea-list.php',
-        type: 'GET',
-        success: function (response){
+      url:'tarea-list.php',
+      type: 'GET',
+      success: function (response){
         //console.log(response);
         let tasks = JSON.parse(response);
         let template = '';
         tasks.forEach(task => {
-            template +=`
+          template +=`
             <tr tareaId="${task.id}">
-               <td >${task.id}</td>
-               <td>
-               <a href='#' class="task-item">${task.nom}  </a>
-               </td>
-               <td>${task.des}</td>
-               <td>
-                  <button class="task-delete">
-                   Borrar
-                  </button>
-               </td>
+              <td >${task.id}</td>
+              <td>
+              <a href='#' class="task-item">${task.entidad}  </a>
+              </td>
+              <td>${task.appat}</td>
+              <td>${task.apmat}</td>
+              <td>${task.nombre}</td>
+              <td>${task.fregis}</td>
+              <td>
+                <button class="task-delete">
+                  Eliminar
+                </button>
+              </td>
             </tr>
-            `
-            });
+          `
+        });
         $('#tareas').html(template);
-        }
-        })
-    }
+      }
+    })
+  }
   // Aqui termina la funcion TraerTareas() ///////////////////
   
-  }); // Termina el cuerpo de ready...
+}); // Termina el cuerpo de ready...
